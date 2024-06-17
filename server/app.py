dd064384d599ef012cf777c33701de1793d7d62c
aboutBlank-dev <josecolaco1999@gmail.com>
2024-06-17 15:52:36 +0100

    changed route from singular to plural

--- server/app.py
+++ server/app.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-from retrieve_map_card_data import get_map_card_data
-from server.models.card import CardSchema
-from server.models.map import MapSchema
 from server.routes.map import router as map_router
 from server.routes.card import router as card_router
 
 app = FastAPI()
-app.include_router(card_router, prefix="/card", tags=["Card"])
-app.include_router(map_router, prefix="/map", tags=["Map"])
+app.include_router(card_router, prefix="/cards", tags=["Cards"])
+app.include_router(map_router, prefix="/maps", tags=["Maps"])
 
 @app.get("/")
 def read_root():
