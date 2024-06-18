0e46043c5000eae0edac73ab75e2fafe84f01fc8
aboutBlank-dev <josecolaco1999@gmail.com>
2024-06-18 16:19:57 +0100

    added CORS handling middleware

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from server.routes.map import router as map_router
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.card import router as card_router
+from server.routes.map import router as map_router
 
 app = FastAPI()
+origins = ["0.0.0.0"]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
 app.include_router(card_router, prefix="/cards", tags=["Cards"])
 app.include_router(map_router, prefix="/maps", tags=["Maps"])
 
